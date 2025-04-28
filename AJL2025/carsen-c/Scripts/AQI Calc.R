8dd36d6acf729d0af37951ea9d9bcc2fb61fcabb
carsen-c <spritemoth@gmail.com>
2025-04-28 15:47:48 -0400

    Update AQI Calc.R

--- AJL2025/carsen-c/Scripts/AQI Calc.R
+++ AJL2025/carsen-c/Scripts/AQI Calc.R
@@ -1,73 +1,100 @@
 library(tidyverse)
 
-library(dplyr)
-library(lubridate)
-library(ggplot2)
+# Last updated 4/28/25.
 
-# Last updated 1/29/25.
 # Setup:
-# Run lines 1-5
-# Set Working Directory to folder with all needed .csv's 
-# Check for standard formatting for each file - if you pull unaltered from 
+# Run line 1
+# Set Working Directory to folder with all needed .csv's:
+# PurpleAir data pulls
+# External climatological reports - if necessary
+
+# Check for standard name formatting for each file - if you pull unaltered from 
 # PurpleAir, you should be good.
 
-# This is a user-hostile script.
+# Create a vector of all sensor indexes, as well as any sub-vectors for
+# desired subgroups.
 
-# Create a vector of all sensor indexes, as well as a vector for the specific 
-# sensor indexes you want to look at. Store as numbers - these will be converted 
-# to strings.
+# This script is written with the sensor indexes corresponding to sensors affiliated
+# with the Air Justice Lab (Sanctuary for Independent Media). These values should
+# be replaced with the indexes of sensors the user wishes to analyze.
 
-sensor_indexes <- c(188595,220769,220793,164327,156051,166257,87173,149970,149966,
-                   166363,151552,166677,166327,166653,151490,151536,166717,156171,
-                   156199,164351,166471)
+sensor_indexes <- c(220769,220793,164327,156051,166257,87173,149970,149966,
+                    166363,151552,166677,166327,166653,151490,151536,166717,156171,
+                    156199,164351,166471)
 
-sensor_indexes.alb <- c(151490,151536,166717,166471,151552,166363,149966,220793)
-# The sensors located in Albany County.
-sensor_indexes.not <- setdiff(sensor_indexes, sensor_indexes.alb)
-# The rest of the sensors.
-
-# NOTE FOR MYSELF: Sensor 188595 did not capture relative humidity data.
-
-# Converts sensor indexes from numeric to character.
 sensor_indexes <- as.character(sensor_indexes)
-sensor_indexes.alb <- as.character(sensor_indexes.alb)
-sensor_indexes.not <- as.character(sensor_indexes.not)
+# Converts each member of sensor_indexes to a character string, in order to read
+# file names in the Working Directory.
 
-# for loop start: this code is run for each index in sensor_indexes, and generates
-# a new .csv file, stored in the Working Directory, tracking Daily AQI per sensor
-# by the EPA's standards, applying the EPA's corrective function for PurpleAir 
-# sensors.
+name_end <- "2025-04-01 2025-04-28 60-Minute Average.csv"
+# This is a specific string corresponding to the end of every file name, when 
+# left in the format PurpleAir's download tool applies.
+# Replace with the string specific to the user's data pull
+
+
+# Some PurpleAir sensors fail to record climatological data such as temperature
+# and relative humidity. Supplemental data may be taken from external sources
+
+# The Albany, NY NWS Station supplied a monthly average relative humidity
+# from April 2024 to March 2025, as well as daily average relative humidity for the
+# month of April.
+
+# Users of this script may use the National Weather Service's website to pull
+# climatological data from a NWS station in the region they are monitoring, or
+# may contact a local station directly to request data. Or a secret third thing.
+
+# External relative humidity data is used when PurpleAir's sensors fail to record 
+# relative humidity. For Albany, NY, during April 2025, the
+# average relative humidity was:
+
+avg_RH <- 61.2
+
+# with a standard deviation of:
+
+stdv_RH <- 11.4
+
+# These values should be adjusted for the date and location of the specific sensors
+# the user wishes to analyze.
+
+
+
+# This for loop iterates through each index in sensor_indexes, applies the EPA's
+# corrective function, and calculates the Daily Air Quality Index (AQI). A
+# complete explanation of why this is needed is available on the Air Justice Lab's
+# GitHub page.
 
 for(i in 1:length(sensor_indexes)){
   index <- sensor_indexes[i]
-  filename <- (paste(index, "2024-12-28 2025-01-28 60-Minute Average.csv", sep = " "))
+  filename <- (paste(index, name_end, sep = " "))
   
   sensor_df <- read.csv(filename)
   
-  # Checks if relative humidity was recorded - if not, value replaced with 0.
-  # A longer explanation for why this is an acceptable estimate is available
-  # by phone at 256-509-4140.
   
   for(i in 1:length(sensor_df$humidity)){
     if(is.na(sensor_df$humidity[i])){
-      sensor_df$humidity[i] <- 0
+      sensor_df$humidity[i] <- avg_RH
     }
   }
-  
-  # By the EPA's standards for correcting PM2.5 measured by PurpleAir's sensors,
-  # take the maximum of PM2.5_cf_1_a and PM2.5_cf_1_b
+  # Checks if relative humidity was recorded - if not, value replaced with the
+  # average relative humidity.
+  # This value for this instance is the average taken over the month of April,
+  # reported by the Albany, NY NWS Station. A complete explanation for why any
+  # particular value is used is available on the AJL's GitHub.
   
   cf_1 <- data.frame(sensor_df$pm2.5_cf_1_a, sensor_df$pm2.5_cf_1_b)
   cf_1$max <- apply(cf_1, 1, max, na.rm = TRUE)
+  # By the EPA's standards for correcting PM2.5 measured by PurpleAir's sensors,
+  # takes the maximum of PM2.5_cf_1_a and PM2.5_cf_1_b
   
-  # Create a new dataframe with all relevant information to apply the EPA's
-  # corrective function
   
   sensor_df.stripped <- data.frame(sensor_df$time_stamp, sensor_df$humidity,
                                    cf_1$max)
-  # Correct PM2.5 values
+  # Create a new dataframe with all relevant information to apply the EPA's
+  # corrective function
+  
   
   PM2.5_correct <- c()
+  # Correct PM2.5 values
   
   for(i in 1:nrow(sensor_df.stripped)){
     pm <- sensor_df.stripped$cf_1.max[i]
@@ -88,13 +115,12 @@ for(i in 1:length(sensor_indexes)){
   
   sensor_df.stripped$PM2.5 <- PM2.5_correct
   
-  # Sort the not-day data into days - this works IF YOU DON'T TOUCH THE RAW CSV
-  # Seriously. Don't touch it. Or, do, but fixing the rest of the code to 
-  # accommodate your mistake is left as an exercise for the reader.
   
   TS_days <- c(as.character(sensor_df.stripped$sensor_df.time_stamp))
   TS_days <- substr(TS_days, start = 1, stop = 10)
   sensor_df.stripped$TS_days <- ymd(TS_days)
+  # Sorts the not-day data into days - this works IF YOU DON'T TOUCH THE RAW CSV
+  # Seriously. Don't touch it.
   
   # These next steps group the data by day and find the daily maximum value of
   # PM2.5 - the value the EPA uses to calculate the Air Quality Index (AQI)
@@ -105,15 +131,14 @@ for(i in 1:length(sensor_indexes)){
     group_by(sensor_df.stripped$TS_days)
   
   daily_max_sensor_df <- data.frame(sensor_df.stripped.grp %>%
-    summarise(max = max(PM2.5)))
+                                      summarise(max = max(PM2.5)))
   
-  # Calculating and storing AQI - referencing the EPA's technical guide, which 
-  # I'll link later.
+  
   
   AQI <- c()
   AQI_cats <- c()
-  
-  # Start of another goddamn for loop
+  # Calculates and stores AQI - referencing the EPA's technical guide. A copy
+  # and link is available on the Sanctuary's GitHub.
   
   for(i in 1:nrow(daily_max_sensor_df)){
     x <- daily_max_sensor_df$max[i]
@@ -196,13 +221,9 @@ for(i in 1:length(sensor_indexes)){
     AQI_cats <- append(AQI_cats, y)
   }
   
-  # Generate a data frame and write it to a unique .csv, to avoid losing work.
-  # To my (limited) knowledge, this loop will overwrite the dataframe each turn.
-  # So, I'm giving this method a shot. Computational efficiency is for chumps.
+ 
   
-  # The new .csv's will be stored in the Working Directory. Please DO NOT
-  # change the working directory mid-script [i.e. by running portions of the
-  # script and switching in between.] I did not and will not plan for this.
+  
   
   sensor_AQI <- data.frame(daily_max_sensor_df$sensor_df.stripped.TS_days, AQI,
                            AQI_cats)
@@ -210,8 +231,11 @@ for(i in 1:length(sensor_indexes)){
   
   write.csv(sensor_AQI, file = (paste(index, "Daily AQI 12-28-2025-01-28.csv", 
                                       sep = " ")))
+  # Generates a data frame and writes it to a unique .csv.
+  # The new .csv's will be stored in the Working Directory. Please DO NOT
+  # change the working directory mid-script [i.e. by running portions of the
+  # script and switching in between.] I did not and will not plan for this.
+  
 }
 
-# The next big project I have in mind for myself is graph generation that isn't
-# hardcoded. But that's for another night, and I'm cutting it off here. 
-# Last updated 1/29/25.
+
